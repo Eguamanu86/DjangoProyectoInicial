6bb562fb55746ce5cde2918d404bbd13d7f17fcd
Ernesto Guaman <eguamanu1@unemi.edu.ec>
2025-05-09 20:23:42 -0500

    bug solucionado

--- app/settings.py
+++ app/settings.py
@@ -82,10 +82,10 @@ DATABASES = {
         'ENGINE': os.getenv("DB_ENGINE"),
         'NAME': os.getenv("DB_DATABASE"),
         'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD_1"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
-        'ATOMIC_REQUESTS': False
+        'ATOMIC_REQUESTS': True
     }
 }
 
