8021253818b062fe04e553549803b2ed03d83056
Ernesto Guaman <eguamanu1@unemi.edu.ec>
2025-06-07 09:18:01 -0500

    se agrega modulos y grupos

--- seguridad/models.py
+++ seguridad/models.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .managers import CustomUserManager
+from crum import get_current_user
+from django.contrib.auth.models import Group
 
 # Create your models here.
 class User(AbstractBaseUser, PermissionsMixin):
@@ -46,3 +48,105 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def get_foto_url(self):
         return self.foto.url
+
+class Modulo(models.Model):
+    CLASE_RECURSO = (
+        ('LISTA', 'LISTA'),
+        ('ITEM', 'ITEM'),
+    )
+    TIPO_RECURSO = (
+        ('Mantenimientos', 'MANTENIMIENTOS'),
+        ('Documentos', 'DOCUMENTOS'),
+        ('Procesos', 'PROCESOS'),
+        ('Informes', 'INFORMES'),
+        ('Seguridad', 'SEGURIDAD'),
+    )
+    padre = models.ForeignKey('self', on_delete=models.PROTECT, blank=True, null=True)
+    codigo = models.CharField(max_length=10, blank=True, null=True)
+    url = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    orden = models.CharField(max_length=1024, blank=True, null=True, editable=False)
+    ruta = models.CharField(max_length=1024, blank=True, null=True, editable=False)
+    clase = models.CharField(max_length=10, blank=True, null=True, choices=CLASE_RECURSO)
+    tipo = models.CharField(max_length=15, blank=True, null=True, choices=TIPO_RECURSO)
+    icono = models.CharField(max_length=100, blank=True, null=True)
+    descripcion = models.CharField(max_length=100, blank=True, null=True)
+    item_orden = models.IntegerField(default=0, blank=True, null=True)
+    habilitado = models.BooleanField(default=False)
+    creadopor = models.CharField(max_length=100, blank=True, null=True, editable=False)
+    editadopor = models.CharField(max_length=100, blank=True, null=True, editable=False)
+    creadodate = models.DateTimeField(auto_now_add=True)
+    editadodate = models.DateTimeField(auto_now=True)
+    pcid = models.CharField(max_length=200, blank=True, null=True, editable=False)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.nombre} (/{self.url})'
+
+    class Meta:
+        verbose_name = 'Módulo'
+        verbose_name_plural = 'Módulos'
+        ordering = ['item_orden']
+
+    def save(self, force_insert=False, force_update=False, using=None, **kwargs):
+        if self.codigo:
+            self.codigo = self.codigo.upper()
+        try:
+            user = get_current_user()
+
+            if self._state.adding:
+                self.creadopor = user.username
+            else:
+                self.editadopor = user.username
+        except:
+            pass
+
+        models.Model.save(self)
+
+        cid = str(self.id).zfill(6)
+
+        if not self.padre is None:
+            self.ruta = self.padre.ruta + '/' + cid
+            self.orden = self.padre.orden + '/' + str(self.nombre)
+        else:
+            self.ruta = 'ROOT/' + cid
+            self.orden = 'General/' + str(self.nombre)
+
+        models.Model.save(self)
+
+class ModuloGrupo(models.Model):
+    nombre = models.CharField(max_length=100, blank=True, null=True)
+    descripcion = models.CharField(max_length=200, blank=True, null=True)
+    modulos = models.ManyToManyField(Modulo)
+    grupos = models.ManyToManyField(Group)
+    prioridad = models.IntegerField(blank=True, null=True)
+    creadopor = models.CharField(max_length=100, blank=True, null=True, editable=False)
+    editadopor = models.CharField(max_length=100, blank=True, null=True, editable=False)
+    creadodate = models.DateTimeField(auto_now_add=True)
+    editadodate = models.DateTimeField(auto_now=True)
+    pcid = models.CharField(max_length=200, blank=True, null=True, editable=False)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.nombre} {self.prioridad}'
+
+    class Meta:
+        verbose_name = 'Grupo de Módulos'
+        verbose_name_plural = 'Grupos de Módulos'
+        ordering = ['prioridad', 'nombre']
+
+    def modulos_activos(self):
+        return self.modulos.filter(activo=True).order_by('item_orden')
+
+    def save(self, force_insert=False, force_update=False, using=None, **kwargs):
+        try:
+            user = get_current_user()
+
+            if self._state.adding:
+                self.creadopor = user.username
+            else:
+                self.editadopor = user.username
+        except:
+            pass
+
+        models.Model.save(self)
